3927942813423f8c4effc0c96bbdf07a1406b010
David Kane <david@dkane.net>
2020-09-11 10:36:52 +0100

    remove printed summary redirect to ccew homepage

--- app.py
+++ app.py
@@ -8,14 +8,12 @@ CCEW_URL = 'https://register-of-charities.charitycommission.gov.uk/'
 
 @app.route('/')
 def homepage():
-    response = browser.open(CCEW_URL)
-    return response.content
+    return redirect(CCEW_URL, code=303)
 
 @app.route('/charity/<regno>')
 def charity(regno):
     browser.open(CCEW_URL + "charity-search")
     browser.select_form('#_uk_gov_ccew_portlet_CharitySearchPortlet_fm')
-    browser.get_current_form().print_summary()
     browser['_uk_gov_ccew_portlet_CharitySearchPortlet_keywords'] = regno
     browser.submit_selected()
     response = browser.find_link(link_text=regno)
