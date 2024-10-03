a453cf94d0dd82983479a22b169e5f223981add5
David Kane <david@dkane.net>
2024-10-03 10:05:42 +0100

    revert previous changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import datetime
-import io
 
 import mechanicalsoup
 from flask import Flask, abort, redirect, request
 
 app = Flask(__name__)
+browser = mechanicalsoup.StatefulBrowser()
 CCEW_URL = "https://register-of-charities.charitycommission.gov.uk/"
 OSCR_URL = "https://www.oscr.org.uk/"
 FTC_URL = "https://findthatcharity.uk/"
@@ -19,10 +19,7 @@ def get_charity_url(regno: str):
             )
         )
     else:
-        return (
-            CCEW_URL
-            + "en/charity-search/-/results/page/1/delta/20/keywords/{}".format(regno)
-        )
+        return CCEW_URL + "charity-details/?regId={}&subId=0".format(regno)
 
 
 class StripLinkText(str):
@@ -43,8 +40,6 @@ def charity(regno: str):
 
 @app.route("/charity/<regno>/accounts/<fyend>")
 def accounts(regno: str, fyend: str):
-    browser = mechanicalsoup.StatefulBrowser()
-
     user_agent = request.headers.get("User-Agent")
     try:
         fyend = datetime.datetime.strptime(fyend, "%Y-%m-%d")
@@ -52,22 +47,9 @@ def accounts(regno: str, fyend: str):
         abort(400, description=str(e))
     charity_url = get_charity_url(regno)
     if charity_url.startswith(CCEW_URL):
-        response = browser.open(charity_url, headers={"User-Agent": user_agent})
+        browser.open(charity_url, headers={"User-Agent": user_agent})
         try:
-            response.raise_for_status()
-            charity_links = browser.links(
-                url_regex=r"en/charity-search/-/charity-details/"
-            )
-            if not charity_links:
-                abort(404, description="Couldn't find charity")
-            response = browser.follow_link(
-                charity_links[0],
-                requests_kwargs=dict(
-                    headers={"User-Agent": user_agent},
-                ),
-            )
-            response.raise_for_status()
-            response = browser.follow_link(
+            browser.follow_link(
                 link_text=StripLinkText("Accounts and annual returns"),
                 requests_kwargs=dict(
                     headers={"User-Agent": user_agent},
@@ -82,22 +64,6 @@ def accounts(regno: str, fyend: str):
                 and (cells[0].string.strip().lower() == "accounts and tar")
                 and (cells[1].text[:30].strip() == fyend.strftime("%d %B %Y"))
             ):
-                dummy_file = io.BytesIO()
-                response = browser.download_link(
-                    tr.find("a"), headers={"User-Agent": user_agent}
-                )
-                response.raise_for_status()
-                dummy_file.write(response.content)
-                dummy_file.seek(0)
-                return app.response_class(
-                    dummy_file.read(),
-                    mimetype="application/pdf",
-                    headers={
-                        "Content-Disposition": 'attachment; filename="{}-{}-accounts.pdf"'.format(
-                            regno,
-                            fyend.strftime("%Y-%m-%d"),
-                        )
-                    },
-                )
+                return redirect(tr.find("a").attrs["href"], code=303)
         abort(404, description="This account could be found for this charity")
     abort(404, description="Cannot access accounts for this charity")
